4118a252db4a0039e5e234058f71af61b891c059
unknown <eliaslawrence.jr@gmail.com>
2021-04-03 21:56:58 -0300

    Side menu: -Removing labels -Adding logout button -User email -Store name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+
   public appPages = [
-    { title: 'Login', url: '/home/Inbox', icon: 'home' },
     { title: 'Minha Loja', url: '/store', icon: 'storefront' },
     { title: 'Produtos', url: '/products', icon: 'grid' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+
   constructor() {}
 }
